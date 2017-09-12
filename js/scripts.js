c451dc452670c22187342b0e0a6437001146af12
Rakhee Gandhi and Valia Dempsey <valia@>
2017-09-12 13:45:10 -0700

    Create project Flashcard with initial content

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#js").click(function() {
+    //$(".def-showing").hide();
+    $("#js-def").show();
+  });
+  $(".click-hide").click(function() {
+    $("#js-def").hide();
+  });
+  $("#operator").click(function() {
+    //$(".def-showing").hide();
+    $("#operator-def").show();
+  });
+  $(".click-hide").click(function() {
+    $("#operator-def").hide();
+  });
+
+});
